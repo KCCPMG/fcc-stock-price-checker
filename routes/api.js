a6a21da40bcd5ac550db81a8709c21af11909056
Glitch (brassy-forest) <none>
2019-10-10 07:54:12 +0000

    👼🎲 Checkpoint ./routes/api.js:6206543/335

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ I can GET /api/stock-prices with form data containing a Nasdaq stock ticker and 
 In stockData, I can see the stock(string, the ticker), price(decimal in string format), and likes(int).
 I can also pass along field like as true(boolean) to have my like added to the stock(s). Only 1 like per ip should be accepted.
 If I pass along 2 stocks, the return object will be an array with both stock's info but instead of likes, it will display rel_likes(the difference between the likes on both) on both.
-A good way to receive current price is the following external API(replacing 'GOOG' with your stock): https://finance.google.com/finance/info?q=NASDAQ%3aGOOG
+x - A good way to receive current price is the following external API(replacing 'GOOG' with your stock): https://finance.google.com/finance/info?q=NASDAQ%3aGOOG
 All 5 functional tests are complete and passing.
 */
 
@@ -46,15 +46,16 @@ module.exports = function (app) {
           if (this.status === 200) {
             // console.log(this.responseType);
             let obj = JSON.parse(this.responseText);
-            if (obj === '{
-    "Error Message": "Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_INTRADAY."
-}')
-            // console.log(typeof obj);
-            // console.log(obj["Meta Data"]);
-            let lastRefreshed = obj["Meta Data"]["3. Last Refreshed"];
-            // console.log(lastRefreshed);
-            // console.log(obj["Time Series (5min)"][lastRefreshed]['4. close']);
-            res.send(obj["Time Series (5min)"][lastRefreshed]['4. close'])
+            if (obj["Error Message"] === "Invalid API call. Please retry or visit the documentation (https://www.alphavantage.co/documentation/) for TIME_SERIES_INTRADAY.") {
+              res.send("invalid stock")
+            } else {
+              // console.log(typeof obj);
+              // console.log(obj["Meta Data"]);
+              let lastRefreshed = obj["Meta Data"]["3. Last Refreshed"];
+              // console.log(lastRefreshed);
+              // console.log(obj["Time Series (5min)"][lastRefreshed]['4. close']);
+              res.send(obj["Time Series (5min)"][lastRefreshed]['4. close'])
+            }
           } else {
             res.send("bad connection");
           }
